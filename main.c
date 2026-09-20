0a64228a6c48256f4ba5456fd4d3427a90c9d02f
asdf51711 <siyeonsong17@sookmyung.ac.kr>
2026-09-21 02:04:58 +0900

    실습4

--- main.c
+++ main.c
@@ -2,12 +2,17 @@
 
 int main(void)
 {
-    char c;
+    int a, b;
+    float result;
 
-    printf("enter a character : ");
-    scanf("%c", &c);
+    printf("분자를 입력하세요 : ");
+    scanf("%d", &a);
 
-    printf("the next character of %c (%i) is %c (%i)\n", c, c, c+1, c+1);
+    printf("분모를 입력하세요 : ");
+    scanf("%d", &b);
+
+    result = (float)a / b;
+    printf("나누기의 결과는 %f 입니다.\n", result);
 
     return 0;
 }
